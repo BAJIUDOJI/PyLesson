9fe30033f9d2a4c2e752f2b50860bf1535f9a89e
PetrOFFsky <validol.se@gmail.com>
2022-06-13 13:45:51 +0300

    init

---
+++ lesson6.py
@@ -0,0 +1,9 @@
+x = 25
+y = 34.5
+print(x / y)
+
+price = 300
+price2 = 2.72
+print(price * price2)
+
+print(type(y))
---
+++ lesson7.py
@@ -0,0 +1,12 @@
+# test = None
+# print(test)
+
+# x, y = (1, 5)
+# print(x, y)
+
+
+# my_true = True
+# my_false = False
+#
+# print(type(my_true))
+
