0c549c8e108e53c4b32bfed113c8921c8adde5e6
BAJIUDOJI <validol.se@gmail.com>
2022-06-24 11:02:07 +0300

    stepik 24.06.22

--- stepik.py
+++ stepik.py
@@ -151,29 +151,107 @@
 # print(m)
 
 
-n, m, k = (int(i) for i in input().split())
-a = [[0 for j in range(m)]for i in range(n)]
-for i in range(k):
-    row, col = (int(i) - 1 for i in input().split())
-    a[row][col] = - 1
-for i in range(n):
-    for j in range(m):
-        if a[i][j] == 0:
-            for di in range(-1, 2):
-                for dj in range(-1, 2):
-                    ai = i + di
-                    aj = j + dj
-                    if 0 <= ai < n and 0 <= aj < m and a[ai][aj] == -1:
-                        a[i][j] += 1
-for i in range(n):
-    for j in range(m):
-        if a[i][j] == -1:
-            print('*', end='')
-        elif a[i][j] == 0:
-            print('.', end='')
-        else:
-            print(a[i][j], end='')
-    print()
+# n, m, k = (int(i) for i in input().split())
+# a = [[0 for j in range(m)]for i in range(n)]
+# for i in range(k):
+#     row, col = (int(i) - 1 for i in input().split())
+#     a[row][col] = - 1
+# for i in range(n):
+#     for j in range(m):
+#         if a[i][j] == 0:
+#             for di in range(-1, 2):
+#                 for dj in range(-1, 2):
+#                     ai = i + di
+#                     aj = j + dj
+#                     if 0 <= ai < n and 0 <= aj < m and a[ai][aj] == -1:
+#                         a[i][j] += 1
+# for i in range(n):
+#     for j in range(m):
+#         if a[i][j] == -1:
+#             print('*', end='')
+#         elif a[i][j] == 0:
+#             print('.', end='')
+#         else:
+#             print(a[i][j], end='')
+#     print()
 
 
+# a1 = int(input())
+# s = a1
+# s2 = 0 + abs(a1 ** 2)
+# while s != 0:
+#     a1 = int(input())
+#     s = s + a1
+#     s2 = s2 + abs(a1) ** 2
+#     if s == 0:
+#         break
+# print(s2)
 
+
+# n = int(input())
+# lis = []
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         lis.append(i)
+# print(*lis[:n])
+
+
+# s = [int(i) for i in input().split()]
+# n = int(input())
+# t = []
+# l = len(s) - 1
+# if n in s:
+#     for i in range(0, l + 1):
+#         if s[i] == n:
+#             t.append(i)
+#     g = len(t) - 1
+#     for j in range(0, g + 1):
+#         print(t[j], end=' ')
+# else:
+#     print('Отсутствует')
+
+
+# m = []
+# while True:
+#     n = input().split()
+#     if n[0] == 'end':
+#         break
+#     else:
+#         m.append(n)
+# for i in range(len(m)):
+#     x = ''
+#     for j in range(len(m[i])):
+#         a = int(m[i - 1][j])
+#         if i == len(m) - 1:
+#             b = int(m[0][j])
+#         else:
+#             b = int(m[i + 1][j])
+#         c = int(m[i][j - 1])
+#         if j == len(m[i]) - 1:
+#             d = int(m[i][0])
+#         else:
+#             d = int(m[i][j + 1])
+#         s = a + b + c + d
+#         x += str(s) + ' '
+#     print(x.rstrip())
+
+
+# a = int(input())
+# nul = [[0] * a for i in range(a)]
+# x, y = 0, 0
+# for i in range(1, a ** 2 + 1):
+#     nul[x][y] = i
+#     if x <= y + 1 and x + y < a - 1:
+#         y += 1
+#     elif x < y:
+#         x += 1
+#     elif x + y >= a:
+#         y -= 1
+#     elif x >= y:
+#         x -= 1
+# for i in range(a):
+#     print(*nul[i])
+
+
+x = {'name': 'John', 'age': 36}
+print(type(x))
